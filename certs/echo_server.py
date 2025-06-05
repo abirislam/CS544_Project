baf26e5ab8e83b486359e33a0fcb270027367c53
abirislam <abirforhire@gmail.com>
2025-06-05 16:08:12 -0400

    halfway?

--- certs/echo_server.py
+++ certs/echo_server.py
@@ -4,20 +4,55 @@ import json
 from echo_quic import EchoQuicConnection, QuicStreamEvent
 import pdu
 
-
+clients = {}
+id_tracker = 1
 
 async def echo_server_proto(scope:Dict, conn:EchoQuicConnection):
+        global id_tracker
         
         message:QuicStreamEvent = await conn.receive()
-        
-        dgram_in = pdu.Datagram.from_bytes(message.data)
-        print("[svr] received message: ", dgram_in.msg)
-        
+        dgram_in = pdu.Message.from_bytes(message.data)
+        print("[svr] received message type: ", dgram_in.mtype)
+        print("[svr] received message: ", dgram_in.payload)
         stream_id = message.stream_id
+
+        if dgram_in.mtype == pdu.LOGIN_REQUEST:
+                username = dgram_in.payload.get("username")
+                password = dgram_in.payload.get("password")
+                print(f"[svr] Login attempt by {username}")
+
+                if username == "test_user" and password == "awesomepassword":
+                        auth = 0
+                        id = id_tracker
+                        clients[id] = conn
+                        id_tracker += 1
+                else:
+                        auth = 1
+                        id = -1
+
+                response = pdu.login_response(auth, id)
+                await conn.send(QuicStreamEvent(stream_id, response.to_bytes(), False))
         
-        dgram_out = dgram_in
-        dgram_out.mtype |= pdu.MSG_TYPE_DATA_ACK
-        dgram_out.msg = "SVR-ACK: " + dgram_out.msg
-        rsp_msg = dgram_out.to_bytes()
-        rsp_evnt = QuicStreamEvent(stream_id, rsp_msg, False)
-        await conn.send(rsp_evnt)
+        elif dgram_in.mtype == pdu.CHAT_MESSAGE:
+                id = dgram_in.payload.get("id")
+                chat_msg = dgram_in.payload.get("message")
+                print(f"[svr] Chat from {id}: {chat_msg}")
+
+                # temporary
+                await conn.send(QuicStreamEvent(stream_id, dgram_in.to_bytes(), False))
+
+        elif dgram_in.mtype == pdu.LOGOUT_MESSAGE:
+                id = dgram_in.payload.get("id")
+                print(f"[svr] Logout by {id}")
+                clients.pop(id)
+
+        else:
+                print("[svr] Ignoring unknown message:", dgram_in.mtype)
+
+
+        # dgram_out = dgram_in
+        # dgram_out.mtype |= pdu.MSG_TYPE_DATA_ACK
+        # dgram_out.msg = "SVR-ACK: " + dgram_out.msg
+        # rsp_msg = dgram_out.to_bytes()
+        # rsp_evnt = QuicStreamEvent(stream_id, rsp_msg, False)
+        # await conn.send(rsp_evnt)
--- echo_client.py
+++ echo_client.py
@@ -3,6 +3,7 @@ from typing import Dict
 import json
 from echo_quic import EchoQuicConnection, QuicStreamEvent
 import pdu
+from time import time
 
 
 async def echo_client_proto(scope:Dict, conn:EchoQuicConnection):
@@ -12,15 +13,45 @@ async def echo_client_proto(scope:Dict, conn:EchoQuicConnection):
 
     # LOGIN_REQUEST
     login_message = pdu.login_request("test_user", "awesomepassword")
-    
     new_stream_id = conn.new_stream()
-
     qs = QuicStreamEvent(new_stream_id, login_message.to_bytes(), False)
     await conn.send(qs)
 
+    # LOGIN_RESPONSE
     message:QuicStreamEvent = await conn.receive()
     response = pdu.Message.from_bytes(message.data)
 
     print('[cli] response type: ', response.mtype)
     print('[cli] payload: ', response.payload)
+
+    if response.mtype != pdu.LOGIN_RESPONSE or response.payload.get("auth") != 0:
+        print("[cli] Login failed.")
+        return
+    
+    id = response.payload["id"]
+
+    # CHAT_MESSAGE
+    some_id = 5577
+    chat = "HELLO I AM THE CLIENT!"
+    current_time = int(time())
+    chat_msg = pdu.chat_message(id, current_time, chat)
+
+    await conn.send(QuicStreamEvent(new_stream_id, chat_msg.to_bytes(), False))
+
+    chat_response: QuicStreamEvent = await conn.receive()
+    chat_response_msg = pdu.Message.from_bytes(chat_response.data)
+
+    print('[cli] chat response type: ', chat_response_msg.mtype)
+    print('[cli] chat payload: ', chat_response_msg.payload)
+
+    # LOGOUT_MESSAGE
+    logout = pdu.logout_message(some_id)
+    logout_event = QuicStreamEvent(conn.new_stream(), logout.to_bytes(), False)
+    await conn.send(logout_event)
+
+    print('[cli] sent logout message')
+
+    # ERROR_MESSAGE
+    
+
     #END CLIENT HERE
